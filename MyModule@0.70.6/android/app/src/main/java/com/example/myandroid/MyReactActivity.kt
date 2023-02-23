eabb0a6f45a82b5e149d5f5c0bf2d560ec598129
Sunbreak <sunbreak.wang@gmail.com>
2023-02-23 20:38:47 +0800

    Use Fabric for MyModule@0.70.6

--- MyModule@0.70.6/android/app/src/main/java/com/example/myandroid/MyReactActivity.kt
+++ MyModule@0.70.6/android/app/src/main/java/com/example/myandroid/MyReactActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactRootView
+import com.facebook.react.bridge.*
 import com.facebook.react.common.LifecycleState
+import com.facebook.react.fabric.ComponentFactory
+import com.facebook.react.fabric.CoreComponentsRegistry
+import com.facebook.react.fabric.FabricJSIModuleProvider
+import com.facebook.react.fabric.ReactNativeConfig
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
 import com.facebook.react.shell.MainReactPackage
+import com.facebook.react.uimanager.ViewManagerRegistry
 import com.facebook.soloader.SoLoader
 
 class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
@@ -18,10 +24,38 @@ class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
         super.onCreate(savedInstanceState)
         SoLoader.init(this, false)
         reactRootView = ReactRootView(this)
+        reactRootView.setIsFabric(true)
         val packages: List<ReactPackage> = listOf(MainReactPackage(null))
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(MyReactNativePackage())
         // Remember to include them in `settings.gradle` and `app/build.gradle` too.
+        val jsiModulePackage = object : JSIModulePackage {
+            lateinit var reactInstanceManager_: ReactInstanceManager
+
+            override fun getJSIModules(
+                reactApplicationContext: ReactApplicationContext,
+                jsContext: JavaScriptContextHolder
+            ): MutableList<JSIModuleSpec<out JSIModule>> {
+                return mutableListOf(object : JSIModuleSpec<UIManager> {
+                    override fun getJSIModuleType() = JSIModuleType.UIManager
+
+                    override fun getJSIModuleProvider(): JSIModuleProvider<UIManager> {
+                        val componentFactory = ComponentFactory()
+                        CoreComponentsRegistry.register(componentFactory)
+                        val viewManagerRegistry = ViewManagerRegistry(
+                            reactInstanceManager_.getOrCreateViewManagers(reactApplicationContext)
+                        )
+                        return FabricJSIModuleProvider(
+                            reactApplicationContext,
+                            componentFactory,
+                            ReactNativeConfig.DEFAULT_CONFIG,
+                            viewManagerRegistry,
+                        )
+                    }
+                })
+            }
+        }
+
         reactInstanceManager = ReactInstanceManager.builder()
             .setApplication(application)
             .setCurrentActivity(this)
@@ -30,8 +64,11 @@ class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
             .addPackages(packages)
             .setUseDeveloperSupport(BuildConfig.DEBUG)
             .setInitialLifecycleState(LifecycleState.RESUMED)
+            .setJSIModulesPackage(jsiModulePackage)
             .setJsEngineAsHermes(true)
             .build()
+
+        jsiModulePackage.reactInstanceManager_ = reactInstanceManager
         // The string here (e.g. "MyReactNativeApp") has to match
         // the string in AppRegistry.registerComponent() in index.js
         reactRootView.startReactApplication(reactInstanceManager, "MyReactNativeApp", null)
