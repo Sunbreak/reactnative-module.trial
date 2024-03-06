fe877e74f774d09b6c9eb1ede3d6c1fc83c82bd9
Sunbreak <sunbreak.wang@gmail.com>
2024-03-06 14:59:10 +0800

    Add react-native module for MyModule@0.73.5

--- MyModule@0.73.5/android/app/build.gradle.kts
+++ MyModule@0.73.5/android/app/build.gradle.kts
@@ -1,6 +1,9 @@
+import groovy.lang.Closure
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.facebook.react")
 }
 
 android {
@@ -30,9 +33,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
@@ -41,7 +41,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // The version of react-native is set by the React Native Gradle Plugin
+    implementation("com.facebook.react:react-android")
+    implementation("com.facebook.react:hermes-android")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+apply(from = "../../node_modules/@react-native-community/cli-platform-android/native_modules.gradle"); (extra["applyNativeModulesAppBuildGradle"] as Closure<*>).call(project)
--- MyModule@0.73.5/android/settings.gradle.kts
+++ MyModule@0.73.5/android/settings.gradle.kts
@@ -1,3 +1,5 @@
+import groovy.lang.Closure
+
 pluginManagement {
     repositories {
         google()
@@ -6,7 +8,7 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
@@ -14,5 +16,7 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "MyAndroid"
+apply(from = "../node_modules/@react-native-community/cli-platform-android/native_modules.gradle"); (extra["applyNativeModulesSettingsGradle"] as Closure<*>).call(settings)
 include(":app")
+includeBuild("../node_modules/@react-native/gradle-plugin")
  
