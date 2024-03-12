7395d813e98fb5240ccb06022b78a8eb00a8fc88
Sunbreak <sunbreak.wang@gmail.com>
2024-03-12 17:19:02 +0800

    Add react-native-safe-area-context for MyModule@0.69.4

--- MyModule@0.69.4/index.js
+++ MyModule@0.69.4/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 import { WebView } from 'react-native-webview';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 // react-native-webview
+//const HelloWorld = () => {
+//  return <WebView source={{ uri: 'https://reactnative.dev/' }} style={{ flex: 1 }} />;
+//};
+
+// react-native-safe-area-context
 const HelloWorld = () => {
-  return <WebView source={{ uri: 'https://reactnative.dev/' }} style={{ flex: 1 }} />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: 'red' }}>
+        <View style={{ flex: 1, backgroundColor: 'blue' }} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 };
 
 var styles = StyleSheet.create({
