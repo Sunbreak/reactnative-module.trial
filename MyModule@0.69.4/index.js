c43fb0d49cb2328fbd9c3b55a01edebd5510e993
Sunbreak <sunbreak.wang@gmail.com>
2024-03-12 21:43:29 +0800

    Add react-native-gesture-handler for MyModule@0.69.4

--- MyModule@0.69.4/index.js
+++ MyModule@0.69.4/index.js
@@ -6,6 +6,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { fetch } from "@react-native-community/netinfo";
+import { GestureHandlerRootView, TapGestureHandler, State } from 'react-native-gesture-handler';
 
 // react-native-webview
 //const HelloWorld = () => {
@@ -79,15 +80,34 @@ import { fetch } from "@react-native-community/netinfo";
 //};
 
 // @react-native-community/netinfo
-const fetchNetInfo = async () => {
-  const { type, isConnected } = await fetch();
-  console.log("type", type, "isConnected", isConnected);
-};
+//const fetchNetInfo = async () => {
+//  const { type, isConnected } = await fetch();
+//  console.log("type", type, "isConnected", isConnected);
+//};
+//const HelloWorld = () => {
+//  return (
+//    <View style={styles.container}>
+//      <Button title="fetchNetInfo" onPress={fetchNetInfo} />
+//    </View>
+//  );
+//};
+
+// react-native-gesture-handler
 const HelloWorld = () => {
+  const onSingleTapEvent = (event) => {
+    if (event.nativeEvent.state === State.ACTIVE) {
+      console.log('Hey single tap!');
+    }
+    console.log('aaa');
+  };
+
   return (
-    <View style={styles.container}>
-      <Button title="fetchNetInfo" onPress={fetchNetInfo} />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <Text>Single Tap Gesture Handler</Text>
+      <TapGestureHandler onHandlerStateChange={onSingleTapEvent}>
+        <View style={styles.square} />
+      </TapGestureHandler>
+    </GestureHandlerRootView>
   );
 };
 
@@ -100,6 +120,12 @@ var styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  square: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#28b5b5',
+    marginTop: 22,
   },
 });
 
