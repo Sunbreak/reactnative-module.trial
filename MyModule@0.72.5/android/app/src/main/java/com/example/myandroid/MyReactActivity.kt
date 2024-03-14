94f767dc84dfeed6b44df711cbf018b1535c7492
Sunbreak <sunbreak.wang@gmail.com>
2024-03-14 16:42:55 +0800

    Use react-native & hermes-engine AAR for MyModule@0.72.5

--- MyModule@0.72.5/android/app/src/main/java/com/example/myandroid/MyReactActivity.kt
+++ MyModule@0.72.5/android/app/src/main/java/com/example/myandroid/MyReactActivity.kt
@@ -2,12 +2,14 @@ package com.example.myandroid
 
 import android.app.Activity
 import android.os.Bundle
-import com.facebook.react.PackageList
+import com.facebook.hermes.reactexecutor.HermesExecutor
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory
 import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactRootView
 import com.facebook.react.common.LifecycleState
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.react.shell.MainReactPackage
 import com.facebook.soloader.SoLoader
 
 class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
@@ -18,7 +20,7 @@ class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
         super.onCreate(savedInstanceState)
         SoLoader.init(this, false)
         reactRootView = ReactRootView(this)
-        val packages: List<ReactPackage> = PackageList(application).packages
+        val packages: List<ReactPackage> = listOf(MainReactPackage(null))
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(MyReactNativePackage())
         // Remember to include them in `settings.gradle` and `app/build.gradle` too.
@@ -30,11 +32,17 @@ class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
             .addPackages(packages)
             .setUseDeveloperSupport(BuildConfig.DEBUG)
             .setInitialLifecycleState(LifecycleState.RESUMED)
+            .setJavaScriptExecutorFactory(getDefaultJSExecutorFactory())
             .build()
         // The string here (e.g. "MyReactNativeApp") has to match
         // the string in AppRegistry.registerComponent() in index.js
         reactRootView.startReactApplication(reactInstanceManager, "MyReactNativeApp", null)
         setContentView(reactRootView)
+    }
+
+    private fun getDefaultJSExecutorFactory(): HermesExecutorFactory {
+        HermesExecutor.loadLibrary()
+        return HermesExecutorFactory()
     }
 
     override fun onDestroy() {
